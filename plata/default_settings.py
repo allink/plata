414fb2349d338ac449838b89178a4707ac3a70a2
bbjay <bbjay@here.local>
2011-02-23 16:38:46 +0100

    implemented setting to disable stock management

--- plata/default_settings.py
+++ plata/default_settings.py
@@ -4,6 +4,7 @@ from django.conf import settings
 
 # Are prices shown with tax included or not?
 PLATA_PRICE_INCLUDES_TAX = getattr(settings, 'PLATA_PRICE_INCLUDES_TAX', True)
+PLATA_STOCK_MANAGMENT_DISABLED = getattr(settings, 'PLATA_STOCK_MANAGMENT_DISABLED', False)
 
 PLATA_ORDER_PROCESSORS = getattr(settings, 'PLATA_ORDER_PROCESSORS', [
     'plata.shop.processors.InitializeOrderProcessor',
